5771740ec7e83b58b4bd7e08e607b431cf071778
Lens Huygh <lens.huygh@gmail.com>
2018-10-29 19:46:30 +0100

    JsonHello : fixed MediaType.APPLICATION_JSON

--- src/main/java/com/lens/ee/rest/JsonHello.java
+++ src/main/java/com/lens/ee/rest/JsonHello.java
@@ -5,20 +5,21 @@ import com.lens.ee.domain.Cat;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("jsonHello")
 public class JsonHello {
     @GET
-    @Produces("application/json")
+    @Produces({MediaType.APPLICATION_JSON})
     public String Hello(){
         return new String("hello json");
     }
 
     @GET
     @Path("cat")
-    @Produces
+    @Produces({MediaType.APPLICATION_JSON})
     public Object getObject(){
         Cat cat = new Cat("Thor",14,"Black");
-        return (Object) cat;
+        return cat;
     }
 }
