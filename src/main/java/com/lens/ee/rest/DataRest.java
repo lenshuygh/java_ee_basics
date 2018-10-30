40d802ee2bb4f7124944aad6aa14ef84a145fc51
Lens Huygh <lens.huygh@gmail.com>
2018-10-30 22:00:29 +0100

    Logging added (sjf4j & logback) in pom log4j.properties added LoggerProducer added to create logger injected logger in DataRest & added logging on 1 addCat()

--- src/main/java/com/lens/ee/rest/DataRest.java
+++ src/main/java/com/lens/ee/rest/DataRest.java
@@ -2,6 +2,7 @@ package com.lens.ee.rest;
 
 import com.lens.ee.domain.Cat;
 import com.lens.ee.service.DataService;
+import org.slf4j.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -14,6 +15,9 @@ public class DataRest {
     @Inject
     private DataService dataService;
 
+    @Inject
+    private Logger logger;
+
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public List<Cat> getCats(){
@@ -24,6 +28,7 @@ public class DataRest {
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public String addCat(Cat cat){
+        logger.debug("Cat -> " + cat);
         dataService.addCatElement(cat);
         return "ok";
     }
